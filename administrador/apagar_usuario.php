728aae3e74191f1ea9279503519bb9684bea7e66
AdelinoViegas <buchoteclas@gmail.com>
2025-03-10 23:33:50 +0100

    removendo espaços

--- administrador/apagar_usuario.php
+++ administrador/apagar_usuario.php
@@ -1,32 +1,23 @@
-<?php 
+<?php
 session_start();
-
 require_once "../conexao.php";
 
-if(!isset($_SESSION['logado'])){
+if (!isset($_SESSION['logado'])) {
   header("Location: ../index.php");
 }
-
 
 $_SESSION['id_user'] = $_POST['id_usuario'];
 $id = $_SESSION['id_user'];
 
-
-
- $sql = "UPDATE usuarios SET estado = '0' WHERE id_usuario = '$id'";
- $result1 = $ligation->prepare($sql);
- $result1->execute();     
- 
+$sql = "UPDATE usuarios SET estado = '0' WHERE id_usuario = '$id'";
+$result1 = $ligation->prepare($sql);
+$result1->execute();
 
 $sql = "UPDATE noticias SET estado = '0' WHERE id_usuario = '$id'";
- $result2 = $ligation->prepare($sql);
- $result2->execute(); 
-  
-  if($result1 AND $result2){
-      header('Location: usuarios.php');
-  }
+$result2 = $ligation->prepare($sql);
+$result2->execute();
 
-
-       
-
- ?>
+if ($result1 and $result2) {
+  header('Location: usuarios.php');
+}
+?>
