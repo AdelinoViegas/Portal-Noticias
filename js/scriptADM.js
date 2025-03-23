5eca7f8c0c035430e84977682f4ff7403f5af569
AdelinoViegas <buchoteclas@gmail.com>
2025-03-23 18:22:40 -0300

    organizando os códigos CSSs

--- js/scriptADM.js
+++ js/scriptADM.js
@@ -1,15 +1,8 @@
-
-window.onresize = ()=>{
-	if(screen.width <= 1090){
-
-	let ul = document.getElementsByClassName('menu_link')[0];
-	let header = document.querySelector('.header');
-
-
-	ul.style.display = "block";
-
-	header.appendChild(ul);
-
+window.onresize = () => {
+	if (screen.width <= 1090) {
+		let ul = document.getElementsByClassName('menu_link')[0];
+		let header = document.querySelector('.header');
+		ul.style.display = "block";
+		header.appendChild(ul);
 	}
-
 }
