3aa8902a493f24d27bf7de78a02420ff0e7bbe44
AdelinoViegas <buchoteclas@gmail.com>
2025-03-28 12:13:31 -0300

    eliminando linhas desnecessárias

--- usuario/home.php
+++ usuario/home.php
@@ -8,10 +8,9 @@ if (!isset($_SESSION['logged']))
 	header("Location: ../index.php");
 
 $categories = getCategories($conection);
-$id = $_SESSION['user_id'];
 $sql = "SELECT * FROM noticies AS n INNER JOIN categories AS c ON n.category_id = c.category_id WHERE _state = '1' AND user_id = :id Limit 1";
 $consult = $conection->prepare($sql);
-$consult->bindParam(":id", $id, PDO::PARAM_STR);
+$consult->bindParam(":id", $_SESSION['user_id'], PDO::PARAM_STR);
 $consult->execute();
 $data = $consult->fetchall(PDO::FETCH_ASSOC)[0];
 ?>
